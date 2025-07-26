1254fec2a37d90cd429d8db99f75666689461069
marinicfede <marinicfede@gmail.com>
2025-07-26 11:36:37 -1000

    fix: correct base path for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/goals-app/',
+  base: '/goals-app-v2/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
